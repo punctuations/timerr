e84691bf601e1a46e1ee6bbc96250bb801e138e7
Matt <matt@dont-ping.me>
2021-04-16 18:18:32 -0700

    fix little problem (temp)

--- pages/dashboard/[did].tsx
+++ pages/dashboard/[did].tsx
@@ -390,7 +390,7 @@ export default function Dashboard(props) {
                       <button
                         type="button"
                         className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                        onClick={() => deleteTimer()}
+                        onClick={() => deleteTimer(0)}
                       >
                         Delete
                       </button>
