9b023238f3f9bd36e611e7bd762f81c1da82c2f4
Matt <matt@dont-ping.me>
2021-04-23 12:29:07 -0700

    fix dashboard
    Co-authored-by: Dries Augustyns <inbox@driaug.com>

---
+++ lib/components/delete.tsx
@@ -0,0 +1,154 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useBoolean } from "react-use";
+
+const Delete = (props: { state: boolean; selectedTimer: string }) => {
+  const [manual, setManualDelete] = useBoolean(true);
+  const [success, setSuccess] = useBoolean(false);
+
+  function deleteTimer() {
+    fetch(
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/api/delete"
+        : "https://timerr.vercel.app/api/delete",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          uuid: props.selectedTimer,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((r) => setSuccess(r.success))
+      .then(() => window.location.reload());
+  }
+
+  return (
+    <AnimatePresence initial={false}>
+      {props.state && manual ? (
+        <motion.div
+          className="fixed z-10 inset-0 overflow-y-auto"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+              aria-hidden="true"
+            />
+
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 text-red-600"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1.5}
+                        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </div>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3
+                      className="text-lg leading-6 font-medium text-gray-900"
+                      id="modal-title"
+                    >
+                      Are you sure you want to to delete the timer?
+                    </h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        The Timer once deleted will be deleted and can not be
+                        done, this action is permanent.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                {success ? (
+                  <button
+                    type="button"
+                    disabled
+                    className="inline-flex w-full justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-400 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-300 sm:ml-3 sm:w-auto sm:text-sm"
+                  >
+                    <svg
+                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      />
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      />
+                    </svg>{" "}
+                    Deleting...
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => deleteTimer()}
+                  >
+                    Delete
+                  </button>
+                )}
+
+                {success ? (
+                  <button
+                    type="button"
+                    disabled
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-200 shadow-sm px-4 py-2 bg-gray-100 text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  >
+                    Cancel
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setManualDelete(false)}
+                  >
+                    Cancel
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      ) : null}
+    </AnimatePresence>
+  );
+};
+
+export default Delete;
---
+++ lib/components/timer.tsx
@@ -0,0 +1,135 @@
+import React from "react";
+import moment from "moment";
+import { CircularProgress } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { useInterval } from "react-use";
+import Delete from "./delete";
+
+const Timer = (props: {
+  timer: {
+    id: number;
+    dash: string;
+    timerUUID: string;
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+    endsAt: string;
+    timeLeft: string;
+    notify: boolean;
+    childLock: boolean;
+  };
+}) => {
+  const router = useRouter();
+  const [delay] = React.useState(1000);
+  const [remaining, setRemaining] = React.useState("");
+  const [progress, setProgress] = React.useState(0);
+  const [isRunning, setIsRunning] = React.useState(true);
+
+  const [deleteState, setDeleteState] = React.useState(false);
+
+  useInterval(
+    () => {
+      setRemaining(moment(props.timer.endsAt).from(new Date().toUTCString()));
+
+      const currentTime = moment().unix();
+
+      const endingTime = moment(props.timer.endsAt).unix();
+      const creationTime = moment(props.timer.createdAt).unix();
+
+      setProgress(
+        moment().unix() > moment(props.timer.endsAt).unix()
+          ? 100
+          : 100 -
+              (Math.abs(currentTime - endingTime) /
+                (Math.abs(currentTime - endingTime) +
+                  currentTime -
+                  creationTime)) *
+                100
+      );
+
+      //toggleIsRunning(Math.trunc(timerStates[0]) !== 100);
+      //setState(Math.trunc(timerStates[0]) === 100);
+    },
+    isRunning ? delay : null
+  );
+
+  return (
+    <>
+      <section className="flex flex-row justify-between px-5 p-4 pb-2">
+        <header className="flex flex-col">
+          <h1 className="text-3xl font-bold">{props.timer.name}</h1>
+          <p className="text-sm">
+            Created on: {new Date(props.timer.createdAt).toLocaleDateString()}
+          </p>
+        </header>
+        <section className="flex flex-row space-x-3 justify-center items-center">
+          <p>
+            {remaining} - {Math.trunc(progress)}%
+          </p>
+          <CircularProgress
+            variant="determinate"
+            value={Math.trunc(progress)}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-gray-500 hover:text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            onClick={() => router.push(`/id/${props.timer.timerUUID}`)}
+          >
+            <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+            <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-red-500 hover:text-red-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            onClick={() => setDeleteState(true)}
+          >
+            <path
+              fillRule="evenodd"
+              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </section>
+      </section>
+      <section className="flex flex-row justify-around border-t border-gray-100 p-4 pt-2">
+        {!props.timer.childLock ? (
+          <div className="inline-flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>{" "}
+            /{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+                clipRule="evenodd"
+              />
+            </svg>{" "}
+          </div>
+        ) : null}
+      </section>
+
+      <Delete state={deleteState} selectedTimer={props.timer.timerUUID} />
+    </>
+  );
+};
+
+export default Timer;
