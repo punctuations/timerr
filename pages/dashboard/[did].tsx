c08b9002bb71316ace19a4092bde7eac14adc12c
Matt <matt@dont-ping.me>
2021-04-27 19:55:52 -0700

    prettier

--- pages/dashboard/[did].tsx
+++ pages/dashboard/[did].tsx
@@ -134,11 +134,11 @@ export default function Dashboard(props) {
             })}
 
             <motion.button
-                variants={container}
-                initial="init"
-                animate="enter"
-                className="w-1/4 transition-colors duration-300 border-gray-200 hover:border-blue-500 hover:text-blue-500 border p-5 rounded-md flex items-center justify-center shadow-md focus:outline-none"
-                onClick={() => router.push(`/${props.prisma[0].dash}/create`)}
+              variants={container}
+              initial="init"
+              animate="enter"
+              className="w-1/4 transition-colors duration-300 border-gray-200 hover:border-blue-500 hover:text-blue-500 border p-5 rounded-md flex items-center justify-center shadow-md focus:outline-none"
+              onClick={() => router.push(`/${props.prisma[0].dash}/create`)}
             >
               Create new &rarr;
             </motion.button>
