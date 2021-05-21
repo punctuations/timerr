e2cc5cbb2341fa2f31cac8e7ee22057392831e27
Matt <matt@dont-ping.me>
2021-05-21 14:29:45 -0700

    add loading icon when clicking button

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import * as crypto from "crypto";
-import { useState, ChangeEvent, useRef, useEffect } from "react";
+import React, { useState, ChangeEvent, useRef, useEffect } from "react";
 import { useKeyPressEvent } from "react-use";
 import { motion } from "framer-motion";
 import { TextField, Checkbox, Select, MenuItem } from "@material-ui/core";
@@ -322,8 +322,28 @@ export default function Home() {
               {submitted ? (
                 <motion.button
                   disabled
-                  className="cursor-not-allowed focus:outline-none transition-colors duration-300 border-gray-200 text-gray-400 border p-3 rounded-md"
+                  className="inline-flex cursor-not-allowed focus:outline-none transition-colors duration-300 border-gray-200 text-gray-400 border p-3 rounded-md"
                 >
+                  <svg
+                    className="animate-spin -ml-1 mr-3 mt-0.5 h-5 w-5 text-gray-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    />
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    />
+                  </svg>{" "}
                   Done &rarr;
                 </motion.button>
               ) : (
